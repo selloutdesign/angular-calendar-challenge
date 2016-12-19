1d406b29899b00c8cfa3a3bd2e09378366b7b64e
selloutdesign <j.selloutdesign@gmail.com>
2016-12-19 00:34:32 -0800

    initial

--- app/app.js
+++ app/app.js
@@ -1,3 +1,30 @@
-angular.module('calendarDemoApp', []);
+var app = angular.module('calendarDemoApp', []);
 
 // your controller and directive code go here
+
+
+app.controller('calCtrl', function($scope) {
+	vm = this;
+	var today = new Date();
+	vm.month = today.getMonth();
+	vm.year = today.getFullYear();
+	vm.years = [];
+	vm.firstYear = vm.year - 20;
+	for(i = 0; i < 41; i++) {
+		vm.years.push(vm.firstYear + i);
+	}
+	vm.selYear = vm.year;
+	console.log(vm.years);
+	console.log(vm.year);
+	vm.months = new Array(	"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+	vm.monthName = vm.months[vm.month];
+
+	vm.computeMonth = function() {
+		console.log(vm.monthName);
+		today.setMonth(vm.months.indexOf(vm.monthName));
+		today.setYear(vm.selYear);
+ 		vm.monthRange = CalendarRange.getMonthlyRange(today);
+	};
+
+	vm.computeMonth();
+});
